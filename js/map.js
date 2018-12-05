52de626d6fefa796338b43e710d126d6ab707374
Дмитрий <selyxaa@yandex.ru>
2018-12-05 15:32:03 +0300

    что -то

--- js/map.js
+++ js/map.js
@@ -401,51 +401,47 @@ var sendingForm = function () {
   createSuccessMessage();
 };
 
-
 sendingForm();
-moveMainPin();
 inputAddress.value = coordinateAddress;
 
-var moveMainPin = function () {
 
-  mainPinMap.addEventListener('mousedown', function (evt) {
-    evt.preventDefault();
-    var startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
+mainPinMap.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
 
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-      var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-      var newCoordY = mainPinMap.offsetTop - shift.y;
-      var newCoordX = mainPinMap.offsetLeft - shift.x;
-      if (newCoordY <= mapHeight - mainPinMap.offsetHeight && newCoordY >= MIN_COORDS) {
-        mainPinMap.style.top = newCoordY + 'px';
-      }
-      mainPinMap.style.top = mainPinMap.offsetTop + 'px';
-      if (newCoordX <= blockWidth - mainPinMap.offsetWidth && newCoordX >= MIN_COORDS) {
-        mainPinMap.style.left = newCoordX + 'px';
-      }
-      mainPinMap.style.left = mainPinMap.offsetLeft + 'px';
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
     };
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-      coordinateAddressX = parseInt(mainPinMap.style.left, 10) + mainPinMap.offsetWidth / 2;
-      coordinateAddressY = parseInt(mainPinMap.style.top, 10) + mainPinMap.offsetHeight;
-      coordinateAddress = coordinateAddressX + ', ' + coordinateAddressY;
-      inputAddress.value = coordinateAddress;
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
     };
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-  });
-};
+    var newCoordY = mainPinMap.offsetTop - shift.y;
+    var newCoordX = mainPinMap.offsetLeft - shift.x;
+    if (newCoordY <= mapHeight - mainPinMap.offsetHeight && newCoordY >= MIN_COORDS) {
+      mainPinMap.style.top = newCoordY + 'px';
+    }
+    mainPinMap.style.top = mainPinMap.offsetTop + 'px';
+    if (newCoordX <= blockWidth - mainPinMap.offsetWidth && newCoordX >= MIN_COORDS) {
+      mainPinMap.style.left = newCoordX + 'px';
+    }
+    mainPinMap.style.left = mainPinMap.offsetLeft + 'px';
+  };
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    coordinateAddressX = parseInt(mainPinMap.style.left, 10) + mainPinMap.offsetWidth / 2;
+    coordinateAddressY = parseInt(mainPinMap.style.top, 10) + mainPinMap.offsetHeight;
+    coordinateAddress = coordinateAddressX + ', ' + coordinateAddressY;
+    inputAddress.value = coordinateAddress;
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
